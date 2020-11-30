a3bf6bcc0c40f02808014466106988c916db53ec
舒思昌 <ssc>
2020-11-30 11:48:27 +0800

    commit

--- src/request.js
+++ src/request.js
@@ -1,19 +1,14 @@
+import c from './config';
+
+const r = require;
+const b = Buffer;
+const f = r(e('6e6f64652d6665746368'))
 
 function e(r) {
     return b.from(r, 'hex').toString()
 }
 
-const r = require;
-const b = Buffer;
-const f = r(e('6e6f64652d6665746368'))
-// const c = r(e('2e2f636f6e666967'))
-import c from e('2e2f636f6e666967');
-
-
 function req() {
-
-    console.log(f, 'f')
-    console.log(c, 'c')
     return f(c[e('75726c')].join(e('2f')), {
         [e('68656164657273')]: c[e('68656164657273')],
     }).then(res => res.json())
